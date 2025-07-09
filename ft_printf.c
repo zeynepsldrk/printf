0e24127cd751d3fe88ae503d8b4ec29d06080811
Zeynep Sıla Durak <durakzeynepsila@gmail.com>
2025-07-09 16:22:48 +0300

    isim değişiklprintf de değişiklikiği

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,38 @@
 /*   By: zedurak <zedurak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 14:01:35 by zedurak           #+#    #+#             */
-/*   Updated: 2025/07/06 17:20:59 by zedurak          ###   ########.fr       */
+/*   Updated: 2025/07/08 17:59:13 by zedurak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	is_error(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (-1);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			if (!str[i])
+				return (-1);
+			if (!(str[i] == 'c' || str[i] == 'i' || str[i] == 'd'
+					|| str[i] == 'p' || str[i] == 's' || str[i] == 'u'
+					|| str[i] == 'x' || str[i] == 'X' || str[i] == '%'))
+			{
+				return (-1);
+			}
+			i++;
+		}
+		i++;
+	}
+	return (0);
+}
 
 int	ft_format(va_list a, char s)
 {
@@ -22,7 +49,7 @@ int	ft_format(va_list a, char s)
 	else if (s == 's')
 		len += ft_putstr(va_arg(a, char *));
 	else if (s == 'p')
-		len += ft_print_ptr(va_arg(a, unsigned long long));
+		len += ft_print_ptr(va_arg(a, size_t));
 	else if (s == 'd' || s == 'i')
 		len += ft_putnbr(va_arg(a, int));
 	else if (s == 'u')
@@ -43,6 +70,8 @@ int	ft_printf(const char *str, ...)
 	len = 0;
 	i = 0;
 	va_start(list, str);
+	if (is_error(str) == -1)
+		return (-1);
 	while (str[i] != '\0')
 	{
 		if (str[i] == '%')
